bfb681a8305bd615db3427634d47b3d4020ab162
Kamachi David <kisaki1kama@gmail.com>
2023-02-21 13:53:07 +0100

    Added environments and configured angular/fire

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDS9nqX98X4WT311HKxqb9xXrwjTTvsUrw",
+        authDomain: "grand-organic.firebaseapp.com",
+        projectId: "grand-organic",
+        storageBucket: "grand-organic.appspot.com",
+        messagingSenderId: "373184225936",
+        appId: "1:373184225936:web:78d76e7da99512250aae15"
+      }
+};
